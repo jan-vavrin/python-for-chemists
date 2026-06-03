56ca9ca1ed490aa8471b405cd15da1b6511f5358
Filip Szczypiński <fiszczyp@gmail.com>
2026-06-03 13:32:19 +0200

    docs: use of LLMs; more resources on MD and LaTeX

--- notebooks/01-notebooks.py
+++ notebooks/01-notebooks.py
@@ -25,11 +25,21 @@ def _():
 
     They can be a mix of _Python cells_ and _Markdown cells_. Python cells contain code that can be run inside the notebook with any output of the code (values, text, plots) appearing directly below.
 
-    Markdown cells (like this one) are used to help to convey information. They are not executed and support formatting in [Markdown](https://www.markdownguide.org/basic-syntax/), as well as some [LaTeX](https://en.wikibooks.org/wiki/LaTeX/Basics):
+    , as well as some
+
+    ### Markdown
+
+    Markdown cells (like this one) are used to help to convey information. They are not executed and support formatting in [Markdown](https://www.markdownguide.org/basic-syntax/). We denote headings with the hash signs – the more `#`, the higher the heading level. Text can be **bolded** with double asterisks (`**text**`) and *emphasised* in italics with single asterisks (`*text*`). You can also make [links](https:/https://duckduckgo.com/) and lists. The guide above gives you more formatting syntax.
+
+    ### LaTeX
+
+    [LaTeX](https://en.wikibooks.org/wiki/LaTeX/Basics) is useful for writing out mathematical equations in your text. We include it in-line with text by encapsulating expressions with dollar signs, e.g. $E=mc^2$ and can also have separated block equations separated between `\[` and `\]`, as below:
 
     \[
             f(x) = 1 + x + \frac{x^2}{2!} + \frac{x^3}{3!} + \ldots.
     \]
+
+    See the source code for this cell for a LaTeX example, otherwise the Internet is a great resource to learn how to use it!
 
     ## Example use
 
@@ -38,6 +48,12 @@ def _():
     ## Explore
 
     To see the Markdown code used to generate this cell, click on the three dots and select "Show code" (or click `Ctrl + H` with the cell highligted). You can also double-click on the text. To add a new cell, click on the small `+` signes to the left. To execute and move to the next cell, press `Shift + Enter`.
+
+    ## AI and LLMs
+
+    Large Language Models have changed the way we code in real life, but they do **not** eliminate the need to understand how coding and basic algorithms function – they are great to help you draft a function or troubleshoot your code, but it is crucial for you to learn how to ask the question in the best possible way. Understanding of the underlying principles or syntax rules helps you maximise the potential of new technologies. Additionally, the skill of abstracting a problem to basic components and algorithms in order to approach it is essential throughout science.
+
+    I recommend that for these workshops you do not use LLMs – there are plenty of links to original resources scatter throughout the notebooks to give you some background information about what we cover.
     """)
     return
 
